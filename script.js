b1dcdb1666ba87e5760f604c1af3e10463145cff
69PercentFat <shubham11501@gmail.com>
2023-09-24 22:29:00 +0530

    Update 2.0
    graph added and css made for mobile friendly use

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let expenses = [];
 
+// Function to add an expense
 function addExpense() {
     const description = document.getElementById('expense-description').value;
     const amount = parseFloat(document.getElementById('expense-amount').value);
@@ -15,12 +16,14 @@ function addExpense() {
         expenses.push(expense);
         updateExpenseList();
         updateSummary();
+        updateSpendingChart();
         clearInputFields();
     } else {
         alert('Please enter a valid description and amount.');
     }
 }
 
+// Function to update the expense list
 function updateExpenseList() {
     const expenseList = document.getElementById('expenses');
     expenseList.innerHTML = '';
@@ -33,6 +36,7 @@ function updateExpenseList() {
     });
 }
 
+// Function to update the summary section
 function updateSummary() {
     const totalSpending = expenses.reduce((total, expense) => total + expense.amount, 0);
     const averageDailySpending = totalSpending / expenses.length || 0;
@@ -41,15 +45,48 @@ function updateSummary() {
     document.getElementById('average-daily-spending').textContent = `₹${averageDailySpending.toFixed(2)}`;
 }
 
+// Function to update the spending chart
+function updateSpendingChart() {
+    const labels = expenses.map(expense => expense.description);
+    const data = expenses.map(expense => expense.amount);
+
+    const ctx = document.getElementById('spending-chart').getContext('2d');
+
+    new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels,
+            datasets: [{
+                label: 'Spending',
+                data,
+                backgroundColor: 'rgba(0, 123, 255, 0.5)', // Blue color
+                borderColor: 'rgba(0, 123, 255, 1)',
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+}
+
+// Function to remove an expense
 function removeExpense(index) {
     expenses.splice(index, 1);
     updateExpenseList();
     updateSummary();
+    updateSpendingChart();
 }
 
+// Function to clear input fields
 function clearInputFields() {
     document.getElementById('expense-description').value = '';
     document.getElementById('expense-amount').value = '';
 }
 
-updateSummary();
+// Call the updateSpendingChart function initially to render the chart
+updateSpendingChart();
